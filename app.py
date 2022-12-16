c9103b0f1a7049836e780d92e99ec7db3654f488
Kinshuk-Goel <kg467@outlook.com>
2022-12-15 19:50:13 -0500

    stuff

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello World"
+
+
+# This is a domain name 
+@app.route('/hello')
+def hello():
+    return "This is a hello statement -- no purpose"
+
+
+
